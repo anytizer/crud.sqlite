8229d0e45df7949a472a1a44c2203d5b27d098b0
server <server@example.com>
2018-08-23 18:46:14 -0600

    js confirm

--- public_html/list.php
+++ public_html/list.php
@@ -2,7 +2,7 @@
 require_once "../library/database.php";
 
 $crud = new crud();
-$students = $crud->students_list();
+$records = $crud->students_list();
 ?>
 <!DOCTYPE html>
 <html>
@@ -23,18 +23,38 @@ $students = $crud->students_list();
 			</tr>
 		</thead>
 		<tbody>
-			<?php foreach($students as $student){?>
+			<?php foreach($records as $record){?>
 			<tr>
-				<td><?php echo $student['name']; ?></td>
-				<td><?php echo $student['email']; ?></td>
+				<td><?php echo $record['name']; ?></td>
+				<td><?php echo $record['email']; ?></td>
 				<td>
-					<a class="w3-btn w3-blue" href="update.php?rid=<?php echo $student['rowid']; ?>">Edit</a> 
-					<a class="w3-btn w3-red"  href="delete.php?rid=<?php echo $student['rowid']; ?>" onclick="return confirm('Are you sure?');">Delete</a>
+					<a class="w3-btn w3-blue" href="update.php?rid=<?php echo $record['rowid']; ?>">Edit</a> 
+					<a class="w3-btn w3-red"  href="delete.php?rid=<?php echo $record['rowid']; ?>">Delete</a>
 				</td>
 			</tr>
 			<?php } ?>
 		</tbody>
 	</table>
 </div>
+<script>
+	function ask(rid=0)
+	{
+		value = confirm("Are you sure to delete this record?");
+		return value;
+	}
+
+	/**
+	 * Attach on-delete
+	 */
+	as = document.getElementsByTagName("a");
+	var length = as.length;
+	for(var i=0; i<length; ++i)
+	{
+		if(as[i].href.match(/delete/g))
+		{
+			as[i].onclick = ask;
+		}
+	}
+</script>
 </body>
 </html>
